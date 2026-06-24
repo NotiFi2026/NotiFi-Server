c2b21f9d17165c71b98e72504fb61f85d8da3319
JaeHyunAn <98042706+yyuneu@users.noreply.github.com>
2026-06-25 01:41:22 +0900

    fix: Swagger 스키마 필드명을 실제 API와 동일한 snake_case로 정렬

--- src/main/java/com/notifi/server/global/config/OpenApiConfig.java
+++ src/main/java/com/notifi/server/global/config/OpenApiConfig.java
@@ -1,5 +1,8 @@
 package com.notifi.server.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import io.swagger.v3.core.jackson.ModelResolver;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -26,5 +29,14 @@ public class OpenApiConfig {
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")));
+    }
+
+    @Bean
+    public ModelResolver modelResolver() {
+        // swagger-core는 Jackson 2(com.fasterxml.jackson)를 사용한다. 앱 런타임의 Jackson 3(tools.jackson)과 별개로
+        // 여기서 Jackson 2 매퍼에 SNAKE_CASE를 지정 → Swagger 스키마 필드명을 실제 API 계약(snake_case)과 일치시킴
+        ObjectMapper swaggerMapper = new ObjectMapper()
+                .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        return new ModelResolver(swaggerMapper);
     }
 }
